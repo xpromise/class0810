8c54f94cece15127031671e3858f0979db0636da
xiongjian <webjsforyou@gmail.com>
2018-11-14 14:47:39 +0800

    jsonp和cors

--- 01.nodejs/day06/02.ajax.js
+++ 01.nodejs/day06/02.ajax.js
@@ -14,6 +14,24 @@ app.post('/testAjax', (req, res) => {
   res.send('这是服务器返回的响应~');
 })
 
+app.get('/testJSONP', (req, res) => {
+  const {callback} = req.query;
+  
+  const data = [{name: 'jack', age: 18}, {name: 'rose', age: 16}];
+  
+  console.log(callback + '(' + JSON.stringify(data) + ')');
+  // getData([{name: 'jack', age: 18}, {name: 'rose', age: 16}])
+  
+  res.end(callback + '(' + JSON.stringify(data) + ')');
+})
+
+app.get('/testCORS', (req, res) => {
+  // res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Origin', 'http://localhost:63342');
+  const data = [{name: 'jack', age: 18}, {name: 'rose', age: 16}];
+  res.json(data);
+})
+
 app.listen(3000, err => {
   if (!err) console.log('服务器启动成功了~');
   else console.log(err);
