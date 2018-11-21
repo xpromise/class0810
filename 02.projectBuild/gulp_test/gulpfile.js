87438c2d3e88246a3e53e96851b0eeceaf798366
xiongjian <webjsforyou@gmail.com>
2018-11-21 11:07:00 +0800

    gulp-css文件的处理

--- 02.projectBuild/gulp_test/gulpfile.js
+++ 02.projectBuild/gulp_test/gulpfile.js
@@ -3,6 +3,8 @@
     1. 是什么？基于流的自动化构建工具
     2. 特点： 基于流和异步构建
     3. 使用：
+      - 英文官网找插件
+        https://gulpjs.com/plugins/
       - 下载插件
         npm i xxx -D
       - 引入插件
@@ -13,6 +15,10 @@
         gulp.task('default', [任务1， 任务2， 任务3...])
     4. 如何启动任务
       gulp 任务名  例如: gulp jshint
+      默认任务  gulp
+    5. gulp任务可以是同步也可以是异步
+      - 加return就是异步
+      - 不加return就是同步
  */
 //引入插件
 const gulp = require('gulp');
@@ -20,6 +26,12 @@ const jshint = require('gulp-jshint');
 const babel = require('gulp-babel');
 const browserify = require('gulp-browserify');
 const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const less = require('gulp-less');
+const LessAutoprefix = require('less-plugin-autoprefix');
+const autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
 
 //配置插件任务
 
@@ -32,21 +44,49 @@ gulp.task('jshint', () => {
     .pipe(jshint.reporter('default'));  //将语法检查的错误在控制台打印输出
 })
 //语法转化
-gulp.task('babel', () =>
-  gulp.src('src/js/*.js')
+gulp.task('babel', ['jshint'], () => {
+  return gulp.src('src/js/*.js')
     .pipe(babel({   //对gulp流中的文件做了语法转化
       presets: ['@babel/env']
     }))
     .pipe(gulp.dest('build/js'))  //将gulp流中的文件输出到指定目录中
-);
+});
 //browserify 将commonjs模块化语法转化为浏览器能识别的语法
-gulp.task('browserify', function() {
+gulp.task('browserify', ['babel'], function() {
   // Single entry point to browserify
-  gulp.src('build/js/index.js')
+  return gulp.src('build/js/index.js')
     .pipe(browserify())  //将commonjs模块化语法转化为浏览器能识别的语法
     .pipe(rename('built.js'))  //将gulp流中的文件重命名
     .pipe(gulp.dest('./build/js')) //将gulp流中的文件输出到指定目录中
 });
+//压缩js
+gulp.task('uglify', ['browserify'], function () {
+  return gulp.src('build/js/built.js')
+    .pipe(uglify())  //压缩js
+    .pipe(rename('dist.min.js'))
+    .pipe(gulp.dest('dist/js'))
+});
+//编译less成css
+gulp.task('less', function () {
+  return gulp.src('./src/less/*.less')
+    .pipe(less({
+      plugins: [autoprefix]
+    }))
+    .pipe(gulp.dest('./build/css'));
+});
+//合并css样式
+gulp.task('concat', ['less'], function() {
+  return gulp.src(['./build/css/test1.css', './build/css/test2.css'])
+    .pipe(concat('built.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+//压缩css
+gulp.task('cssmin', ['concat'], function () {
+  gulp.src('build/css/built.css')
+    .pipe(cssmin())
+    .pipe(rename('dist.min.css'))
+    .pipe(gulp.dest('dist/css'));
+});
 
 //配置默认任务
-gulp.task('default', ['jshint', 'babel', 'browserify'])
+gulp.task('default', ['uglify', 'cssmin'])
