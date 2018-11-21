981a21b6ec765a4b1a69bc0fa637b34f94c8e578
xiongjian <webjsforyou@gmail.com>
2018-11-21 15:58:44 +0800

    webpack-图片、js、html资源的处理

--- 02.projectBuild/webpack_test/webpack.config.js
+++ 02.projectBuild/webpack_test/webpack.config.js
@@ -5,10 +5,17 @@
       1. 官网找loader，找不到再去社区npm找
       2. 先下载，注意下载全所有包
       3. 不需要引入，直接配置
+      
+    插件的使用：
+      1. 官网找插件
+      2. 下载插件
+      3. 引入插件
+      4. 配置插件
    
    如何运行：在项目的根目录运行 webpack （默认会找配置文件）
  */
 const {resolve} = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   //入口起点
@@ -30,9 +37,62 @@ module.exports = {
         }, {
           loader: "less-loader" // 编译less为css
         }]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,   // 8kb以下的图片会做base64处理
+              publicPath: '../build/images',  //修改样式中url图片路径
+              outputPath: 'images',  //图片最终输入的路径
+              name: '[hash:10].[ext]'  //hash 文件哈希值（可以指定位数）  ext 文件扩展名
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/, // 涵盖 .js 文件
+        enforce: "pre", // 预先加载好 jshint loader
+        exclude: /node_modules/, // 排除掉 node_modules 文件夹下的所有文件
+        use: [
+          {
+            loader: "jshint-loader",
+            options: {
+              // 查询 jslint 配置项，请参考 http://www.jshint.com/docs/options/
+              // 例如
+              camelcase: true,
+              //jslint 的错误信息在默认情况下会显示为 warning（警告）类信息
+              //将 emitErrors 参数设置为 true 可使错误显示为 error（错误）类信息
+              emitErrors: true,
+              //jshint 默认情况下不会打断webpack编译
+              //如果你想在 jshint 出现错误时，立刻停止编译
+              //请设置 failOnHint 参数为true
+              failOnHint: true,
+              esversion: 6
+              // 自定义报告函数
+              // reporter: function(errors) { }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   },
   //插件
-  plugins: []
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'   //以指定文件为模板文件，创建新文件，新文件会将打包的资源全部引入
+    }),
+  ]
 }
