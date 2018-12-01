4efa45ee87944fd0e5220e8a444b127ace478361
xiongjian <webjsforyou@gmail.com>
2018-12-01 12:02:12 +0800

    antd-mobile的按需加载功能

--- 03.react/04.react_ui/src/index.js
+++ 03.react/04.react_ui/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 //引入antd-mobile全局样式
-import 'antd-mobile/dist/antd-mobile.min.css';
+// import 'antd-mobile/dist/antd-mobile.min.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
